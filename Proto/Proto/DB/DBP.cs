87164ac41ecb1bdb6d338e02e3a85bc0bbc86fe4
junhyeok <visual713@gmail.com>
2014-11-13 19:35:21 -0600

    sqlite added

--- Proto/Proto/DB/DBP.cs
+++ Proto/Proto/DB/DBP.cs
@@ -8,6 +8,21 @@ namespace Proto.DB
 {
     class DBP
     {
-        
+        string connectionString = "Data Source=MODB.sdb";
+
+        public DBP()
+        {
+            using(System.Data.SQLite.SQLiteConnection con = new System.Data.SQLite.SQLiteConnection(connectionString))
+            {
+                using(System.Data.SQLite.SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(con))
+                {
+                    con.Open();
+
+
+
+                }
+            }
+        }
+
     }
 }
